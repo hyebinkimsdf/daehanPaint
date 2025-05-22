c7ba228bb8dc188fcbf7fc33285db0e4231606ca
연구개발전담부 <wdpb@mig.kr>
2025-05-22 10:32:53 +0900

    fix: 인사말, 영업안내 페이지 공통 디자인 폼 통일화

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <main className="max-w-[1400px] mx-auto px-4 sm:px-6 py-8 sm:py-12">
+    <main className="max-w-[1440px] mx-auto px-4 sm:px-6 py-8 sm:py-12">
       {/* 이미지 + 텍스트 묶음 */}
       <section className="flex flex-col md:flex-row justify-center items-start gap-8  md:mb-16">
         {/* 이미지 */}
@@ -13,12 +13,14 @@ export default function About() {
 
         {/* 텍스트 */}
         <article className="flex flex-col w-full md:w-1/2">
-          <h1 className="text-3xl text-sky-900 font-medium mb-4">안녕하십니까?</h1>
-          <h2 className="text-xl md:text-2xl text-sky-900 mb-6">저희 스타코 대한도료를 방문해주신 모든 고객님들께 감사드립니다.</h2>
+          <h1 className="text-3xl text-sky-900 font-semibold mb-4">안녕하십니까?</h1>
+          <h2 className="text-xl md:text-2xl text-sky-900 mb-6 max-w-[600px]">저희 스타코 대한도료를 방문해주신 모든 고객님들께 감사드립니다.</h2>
           <p className="md:text-base text-sm mb-10 whitespace-pre-line leading-relaxed text-gray-700">
             우리 업체는 언제나 고객 여러분들의 믿음과 신뢰를 바탕으로 스타코시공 전문업체로서, 오랜 경험을 가진 전문팀이 성실하고 꼼꼼한 시공 업무 및 철저한 사후관리로 고객 여러분들을 모시고 있습니다.
+            <br /> <br />
             또한 전문 기술 인력과 최신 장비를 다량 보유하고 있으며, 철저한 프로 정신 아래 정확하게 하자 원인을 분석하여 시공하고 있습니다. 언제나 성실과 신용으로 고객을 위해 최선을 다하는 스타코
-            대한도료가 되도록 노력하겠습니다. 앞으로도 많은 관심과 사랑 부탁드립니다. 감사합니다.
+            대한도료가 되도록 노력하겠습니다. <br /> <br />
+            앞으로도 많은 관심과 사랑 부탁드립니다. 감사합니다.
           </p>
         </article>
       </section>
--- app/business/page.tsx
+++ app/business/page.tsx
@@ -3,24 +3,26 @@ import Image from "next/image";
 
 export default function Business() {
   return (
-    <div className="flex flex-col max-w-[1400px] mx-auto px-4 md:gap-8">
-      <div className="flex flex-col md:flex-row justify-start items-start gap-8 mt-12 max-w-[1400px] mx-auto">
+    <div className="flex flex-col max-w-[1440px] mx-auto px-4 sm:px-6 md:gap-8">
+      <div className="flex flex-col md:flex-row justify-start items-start gap-8 mt-12 max-w-[1440px] mx-auto">
         <div className="w-full md:w-1/2 relative">
-          <Image src="/about/hero.webp" alt="횡계 리치몬드빌라" layout="responsive" width={1200} height={800} className="object-cover rounded-md" />
+          <Image src="/about/hero.webp" alt="횡계 리치몬드빌라" layout="responsive" width={1200} height={800} className="object-cover rounded-lg shadow-md" />
         </div>
         <div className="flex flex-col w-full md:w-1/2">
-          <h2 className="text-3xl text-sky-900 font-medium mb-4">스타코 대한도료 주요업무를 소개합니다</h2>
+          <h2 className="text-3xl text-sky-900 font-semibold mb-4">스타코 대한도료 주요업무를 소개합니다</h2>
           <p className="text-xl md:text-2xl text-sky-900 mb-6">STUCOFLEX·플레시텍스·일반스타코(데코, 노말, 그래뉼)파렉스 스타코시공업체·자제판매</p>
           <p className="md:text-base text-sm mb-10 whitespace-pre-line">
-            우리 업체는 언제나 고객 여러분들의 믿음과 신뢰를 바탕으로 스타코시공 전문업체로서, 오랜 경험을 가진 전문팀이 성실하고 꼼꼼한 시공 업무 및 철저한 사후관리로 고객 여러분들을 모시고 있습니다.
+            우리 업체는 언제나 고객 여러분들의 믿음과 신뢰를 바탕으로 스타코시공 전문업체로서, 오랜 경험을 가진 전문팀이 성실하고 꼼꼼한 시공 업무 및 철저한 사후관리로 고객 여러분들을 모시고 있습니다.{" "}
+            <br /> <br />
             또한 전문 기술 인력과 최신 장비를 다량 보유하고 있으며, 철저한 프로 정신 아래 정확하게 하자 원인을 분석하여 시공하고 있습니다. 언제나 성실과 신용으로 고객을 위해 최선을 다하는 스타코
-            대한도료가 되도록 노력하겠습니다. 앞으로도 많은 관심과 사랑 부탁드립니다. 감사합니다.
+            대한도료가 되도록 노력하겠습니다. <br /> <br />
+            앞으로도 많은 관심과 사랑 부탁드립니다. 감사합니다.
           </p>
         </div>
       </div>
 
       {/* 이미지리스트 */}
-      <div className="flex flex-col md:flex-row gap-4 max-w-[1400px] w-full px-4 mx-auto mb-4">
+      <div className="flex flex-col md:flex-row gap-4 max-w-[1440px] w-full  mx-auto mb-4">
         {[1, 2, 3].map((num) => (
           <div key={num} className="w-full ">
             <Image
