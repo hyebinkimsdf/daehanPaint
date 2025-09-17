34382ded0573dcf0524f8d6f36886f35967a0bfa
globalmig <wdpb@mig.kr>
2025-09-17 15:55:10 +0900

    seo 최적화

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,22 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "스타코 대한도료",
   description: "스타코 시공전문 업체",
+  openGraph: {
+    title: '스타코 대한도료',
+    description: '하자없는 완벽한 스타코 시공 보장! 전문 인력과 최신 장비를 바탕으로 꼼꼼한 시공과 철저한 사후 관리를 제공합니다.',
+    url: 'https://www.stuccodaehandoryo.com/',
+    siteName: '스타코 대한도료',
+    images: [
+      {
+        url: 'https://www.stuccodaehandoryo.com/og_image.png',
+        width: 1200,
+        height: 630,
+        alt: '스타코 대한도료',
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
